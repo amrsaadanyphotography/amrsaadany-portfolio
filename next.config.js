cc7a8c26aa21d80219a3e0c32ddb6e69f7b40e4e
amrsaadanyphotography <46939054+amrsaadanyphotography@users.noreply.github.com>
2025-03-02 11:39:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+// next.config.js
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ['cdn.sanity.io'], // Add your Sanity CDN domain here
   },
 };
