1d017550f8bbe940c115f2d6cee643b6ead0db36
amrsaadanyphotography <46939054+amrsaadanyphotography@users.noreply.github.com>
2025-03-02 11:24:30 +0200

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -1,12 +1,14 @@
-import { createClient } from 'next-sanity';
+// sanity.js
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const sanityClient = createClient({
-  projectId: "nw6fltyv",
-  dataset: "production",
-  useCdn: true,
-  apiVersion: "2023-01-01",
+  projectId: 'nw6fltyv', // Replace with your actual Sanity project ID
+  dataset: 'production', // Use the dataset name you created in Sanity
+  apiVersion: '2023-10-22', // Use a specific API version
+  useCdn: true, // Enable CDN for faster image loading
 });
 
 const builder = imageUrlBuilder(sanityClient);
+
 export const urlFor = (source) => builder.image(source);
